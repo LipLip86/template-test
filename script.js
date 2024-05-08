128a90e7f2efd68905eb24292c7bdd4d6e005ba5
LipLip86 <n.lipchanskaya@gmail.com>
2024-05-08 17:47:39 +0200

    setatrributes fucntion added

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const count = 10;
 const apiKey = "kcme_iAXqp8TdGpC--KAIEVoZM2bkWbVhrp-TrRcAcg";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
-let imgContainer = document.getElementById('image-container')
+let imgContainer = document.getElementById('image-container');
 
+
+// Set attributes fucntion 
+function setAttributes(element, attributes ) {
+    for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
 
 // display photos from API array
 function displayPhotos() {
@@ -10,12 +17,17 @@ function displayPhotos() {
         // Create an element
         let item = document.createElement('a');
         // Set an Attribute
-        item.setAttribute('href', photo.links.html)
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
 
         let img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_title
+        });
 
         // Put the child into parent containers
         item.appendChild(img);
@@ -23,13 +35,11 @@ function displayPhotos() {
     });
 }
 // Get photos from Unsplash Api 
-
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos()
-        // console.log(photosArray);
     }
     catch(error) {
     }
